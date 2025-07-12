3ce674fe109446341a2fd310802e52ee71a39250
Alexis Szerman <alexszer1986@gmail.com>
2025-07-12 16:30:17 -0300

    pwa 001

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon-32x32.png',
+        'favicon-16x16.png',
+        'logo-vymc.svg',
+        'apple-touch-icon.png',
+        'icons/favicon-192x192.png',
+        'icons/favicon-512x512.png'
+      ],
+      manifest: {
+        name: 'Gestor de Reunión Vida y Ministerio Cristiano',
+        short_name: 'VMC',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#1e1e1e',
+        background_color: '#1e1e1e',
+        icons: [
+          {
+            src: '/icons/favicon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
